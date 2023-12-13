16888f8d7ec612f4ba484919bf0789baae17692f
H-mD <syahrizalpmg@gmail.com>
2023-12-13 23:32:51 +0700

    fix: merge user and admin index

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include 'db.php';
 session_start();
 
-if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
+if (!isset($_SESSION['user_role'])) {
     header("Location: login.php");
-    exit(); // Stop further execution of the current page
+    exit();
 }
 ?>
 <!doctype html>
@@ -19,7 +19,6 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <link rel="shortcut icon" href="img/laundry.png" sizes="16x16 32x32" type="image/png">
 
-
   <!--css Native Sendiri-->
   <link rel="stylesheet" href="style.css">
 
@@ -27,7 +26,6 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
 </head>
 
 <body>
-
   <!-- ini navbar Bosq-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
@@ -36,21 +34,33 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <?php if ($_SESSION['user_role'] == 'admin') { ?>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="harga.php">Harga</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                Data Customer
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="minggu.php">Data Minggu Ini</a></li>
+                <li><a class="dropdown-item" href="bulan.php">Data Bulan Ini</a></li>
+                <li><a class="dropdown-item" href="tahun.php">Data Tahun Ini</a></li>
+              </ul>
+            </li>
+          </ul>
+        <?php } ?>
         <!-- Jika tombol logout diletakkan di dalam file yang berbeda -->
         <a href="logout.php" class="btn btn-danger">Logout</a>
       </div>
-
-
-
     </div>
     <?php
-    echo "<font class='ml-3 mr-3' color='white' size='4px'>" . date('l,d-m-y ');
-    echo "</font>";
+      echo "<font class='ml-3 mr-3' color='white' size='4px'>" . date('l,d-m-y ');
+      echo "</font>";
     ?>
   </nav>
   <!-- Ini Akhir Navbar Bosq-->
-
-
 
   <!--ini Konten Bosq-->
   <div class="container">
@@ -63,7 +73,7 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
         <div class="card shadow mb-4">
           <!-- Card Header - Dropdown -->
           <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="m-0 font-weight-bold text-primary">Tambah Costumer:</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Buat Pesanan:</h6>
           </div>
           <!-- Card Body -->
           <div class="card-body">
@@ -76,7 +86,6 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
                   <label>Nama:</label>
                   <input class="form-control form-control-sm" type="text" placeholder="Nama Costumer" aria-label=".form-control-sm example" name='nama' required>
                 </div>
-
 
                 <div class="col-sm-4">
                   <label>Alamat:</label>
@@ -91,7 +100,6 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
 
               </div>
 
-
               <div class="row">
 
                 <div class="col-sm-4">
@@ -99,14 +107,11 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
                   <input class="form-control form-control-sm" type="number" placeholder="Berat" aria-label=".form-control-sm example" name='berat' required>
                 </div>
 
-
-
                 <div class="col-sm-4">
                   <label>Jenis Barang:</label>
                   <select class="form-select form-select-sm" aria-label=".form-select-sm example" name='jenis' required>
                     <option value="pakaian">Pakaian</option>
                     <option value="Boneka">Boneka</option>
-                    <option value="Furniture">Furniture</option>
                     <option value="Lainnya">Lainnya</option>
                   </select>
 
@@ -120,58 +125,46 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
 
               </div>
 
-
-
               <button type="submit" class="btn btn-info btn-lg btn-block mt-4" name='kirim'>Kirim</button>
-
 
               <?php
 
-              $panggil = mysqli_query($conn, "SELECT * FROM harga");
-              $sip = mysqli_fetch_array($panggil);
+                $panggil = mysqli_query($conn, "SELECT * FROM harga");
+                $sip = mysqli_fetch_array($panggil);
 
-              if (isset($_POST['kirim'])) {
+                if (isset($_POST['kirim'])) {
 
-                $nama = $_POST['nama'];
-                $alamat = $_POST['alamat'];
-                $nomor = $_POST['nomor'];
-                $berat = $_POST['berat'];
-                $jenis = $_POST['jenis'];
-                $tanggal = $_POST['tanggal'];
-                $dut = $sip['harga'];
-                $jumlah = $berat * $dut;
+                  $nama = $_POST['nama'];
+                  $nama = mysqli_real_escape_string($conn, $nama);
+                  $alamat = $_POST['alamat'];
+                  $alamat = mysqli_real_escape_string($conn, $alamat);
+                  $nomor = $_POST['nomor'];
+                  $berat = $_POST['berat'];
+                  $jenis = $_POST['jenis'];
+                  $tanggal = $_POST['tanggal'];
+                  $dut = $sip['harga'];
+                  $jumlah = $berat * $dut;
 
+                  $insert = mysqli_query($conn, "INSERT INTO data_minggu 
+                              VALUES (NULL, '$nama', '$alamat', '$nomor', '$berat', '$jenis', '$tanggal', '$jumlah')");
 
-                $insert = mysqli_query($conn, "INSERT INTO data_minggu VALUES (
-                NULL,
-                '$nama',
-                '$alamat',
-                '$nomor',
-                '$berat',
-                '$jenis',
-                '$tanggal',
-                '$jumlah'
-                  )");
-
-                if ($insert) {
-                  echo "<div class='col-md-10 col-sm-12 col-xs-12'>";
-                  echo "<div class='alert alert-primary mt-4 ml-5' role='alert'>";
-                  echo "<p><center>Data Sudah Masuk</center></p>";
-                  echo   "</div>";
-                  echo "</div>";
-                } else {
-                  echo "<div class='col-md-10 col-sm-12 col-xs-12'>";
-                  echo "<div class='alert alert-danger mt-4 ml-5' role='alert'>";
-                  echo "<p><center>Data Gagal Masuk</center></p>";
-                  echo   "</div>";
-                  echo "</div>";
+                  if ($insert) {
+                    echo "<center><div class='col-md-10 col-sm-12 col-xs-12'>";
+                    echo "  <div class='alert alert-primary mt-4 ml-5' role='alert'>";
+                    echo "    <p><center>Data Sudah Masuk</center></p>";
+                    echo "  </div>";
+                    echo "</div><center>";
+                  } 
+                  else {
+                    echo "<center><div class='col-md-10 col-sm-12 col-xs-12'>";
+                    echo "  <div class='alert alert-danger mt-4 ml-5' role='alert'>";
+                    echo "    <p><center>Data Gagal Masuk</center></p>";
+                    echo "  </div>";
+                    echo "</div><center>";
+                  }
                 }
-              }
-
 
               ?>
-
-
 
           </div>
         </div>
@@ -179,15 +172,9 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
     </div>
   </div>
 
-
-
-
-
   </form>
   </div>
   <!--ini Akhir konten bosq-->
-
-
 
   <!-- Optional JavaScript -->
   <!-- Popper.js first, then Bootstrap JS -->
