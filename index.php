157c9ed7e71c4586a1bba792461841ddbc6d8cbc
H-mD <syahrizalpmg@gmail.com>
2023-12-14 08:42:00 +0700

    feat: list pesanan user

--- index.php
+++ index.php
@@ -20,14 +20,14 @@ if (!isset($_SESSION['user_role'])) {
   <link rel="shortcut icon" href="img/laundry.png" sizes="16x16 32x32" type="image/png">
 
   <!--css Native Sendiri-->
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css" type="text/css">
 
   <title>Laundry</title>
 </head>
 
 <body>
   <!-- ini navbar Bosq-->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
     <div class="container-fluid">
       <a class="navbar-brand text-white" href="index.php">Laundryku</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -52,22 +52,26 @@ if (!isset($_SESSION['user_role'])) {
           </ul>
         <?php } ?>
         <!-- Jika tombol logout diletakkan di dalam file yang berbeda -->
+        <!-- <a href="logout.php" class="btn btn-danger">Logout</a> -->
+      </div>
+    </div>
+    <div class="container-fluid d-flex justify-content-end">
+      <div class="tanggal">
+        <?php
+          echo "<font class='ml-3 mr-3' color='white' size='4px'>" . date('l,d-m-y ');
+          echo "</font>";
+        ?>
+      </div>
+      <div class="metu">
         <a href="logout.php" class="btn btn-danger">Logout</a>
       </div>
     </div>
-    <?php
-      echo "<font class='ml-3 mr-3' color='white' size='4px'>" . date('l,d-m-y ');
-      echo "</font>";
-    ?>
   </nav>
   <!-- Ini Akhir Navbar Bosq-->
 
   <!--ini Konten Bosq-->
   <div class="container">
-
     <div class="row mt-5">
-      <div class="col-xl-1">
-      </div>
 
       <div class="col-xl-12 col-lg-8">
         <div class="card shadow mb-4">
@@ -83,19 +87,18 @@ if (!isset($_SESSION['user_role'])) {
               <div class="row">
 
                 <div class="col-sm-4">
-                  <label>Nama:</label>
-                  <input class="form-control form-control-sm" type="text" placeholder="Nama Costumer" aria-label=".form-control-sm example" name='nama' required>
-                </div>
-
-                <div class="col-sm-4">
                   <label>Alamat:</label>
                   <input class="form-control form-control-sm" type="text" placeholder="Kompleks-Desa-Kec-Kab" aria-label=".form-control-sm example" name='alamat' required>
                 </div>
 
-
                 <div class="col-sm-4">
                   <label>Nomor Handphone:</label>
                   <input class="form-control form-control-sm" type="number" placeholder="Nomor Handphone" aria-label=".form-control-sm example" name='nomor' required>
+                </div>
+
+                <div class="col-sm-4">
+                  <label>Tanggal:</label>
+                  <input class="form-control form-control-sm" type="date" placeholder="Berat" aria-label=".form-control-sm example" name='tanggal' required>
                 </div>
 
               </div>
@@ -117,12 +120,6 @@ if (!isset($_SESSION['user_role'])) {
 
                 </div>
 
-                <div class="col-sm-4">
-                  <label>Tanggal:</label>
-                  <input class="form-control form-control-sm" type="date" placeholder="Berat" aria-label=".form-control-sm example" name='tanggal' required>
-
-                </div>
-
               </div>
 
               <button type="submit" class="btn btn-info btn-lg btn-block mt-4" name='kirim'>Kirim</button>
@@ -133,9 +130,7 @@ if (!isset($_SESSION['user_role'])) {
                 $sip = mysqli_fetch_array($panggil);
 
                 if (isset($_POST['kirim'])) {
-
-                  $nama = $_POST['nama'];
-                  $nama = mysqli_real_escape_string($conn, $nama);
+                  $user_id = $_SESSION['user_id'];
                   $alamat = $_POST['alamat'];
                   $alamat = mysqli_real_escape_string($conn, $alamat);
                   $nomor = $_POST['nomor'];
@@ -146,33 +141,80 @@ if (!isset($_SESSION['user_role'])) {
                   $jumlah = $berat * $dut;
 
                   $insert = mysqli_query($conn, "INSERT INTO data_minggu 
-                              VALUES (NULL, '$nama', '$alamat', '$nomor', '$berat', '$jenis', '$tanggal', '$jumlah')");
+                              VALUES (NULL, '$user_id', '$alamat', '$nomor', '$berat', '$jenis', '$tanggal', '$jumlah')");
 
                   if ($insert) {
-                    echo "<center><div class='col-md-10 col-sm-12 col-xs-12'>";
-                    echo "  <div class='alert alert-primary mt-4 ml-5' role='alert'>";
-                    echo "    <p><center>Data Sudah Masuk</center></p>";
-                    echo "  </div>";
-                    echo "</div><center>";
+                    echo "<div class='alert alert-primary mt-4 text-center' role='alert'>Data Sudah Masuk</div>";
                   } 
                   else {
-                    echo "<center><div class='col-md-10 col-sm-12 col-xs-12'>";
-                    echo "  <div class='alert alert-danger mt-4 ml-5' role='alert'>";
-                    echo "    <p><center>Data Gagal Masuk</center></p>";
-                    echo "  </div>";
-                    echo "</div><center>";
+                    echo "<div class='alert alert-danger mt-4 text-center' role='alert'>Data Gagal Masuk</div>";
                   }
                 }
 
               ?>
-
+            </form>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 
-  </form>
+      <div class="col-xl-12 col-lg-8">
+        <div class="card shadow mb-4">
+          <!-- Card Header - Dropdown -->
+          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Status Pesanan:</h6>
+          </div>
+          <!-- Card Body -->
+          <div class="card-body">
+            <div class="table-responsive service">
+              <table class="table table-bordered table-hover  mt-3 text-nowrap css-serial">
+                <tr>
+                  <td><center>No.</td>
+                  <td><center>Nama</td>
+                  <td><center>Alamat</td>
+                  <td><center>Nomor Hp</td>
+                  <td><center>Berat</td>
+                  <td><center>Jenis Barang</td>
+                  <td><center>Tanggal</td>
+                  <td><center>Total</td>
+                </tr>
+
+                <?php
+                  $user_id = $_SESSION['user_id'];
+                  $query = mysqli_query($conn, "SELECT data_minggu.*, users.username FROM data_minggu JOIN users ON data_minggu.user_id = users.id WHERE data_minggu.user_id = $user_id");
+
+                  if (mysqli_num_rows($query) <= 0) {
+                    echo "<div class='col-md-10 col-sm-12 col-xs-12 ml-5'>";
+                    echo "<div class='alert alert-danger mt-4 ml-5' role='alert'>";
+                    echo "<p><center>Data Anda Masih Kosong</center></p>";
+                    echo "</div>";
+                    echo "</div>";
+                  } 
+                  else {
+                    $index = 1;
+                    while ($row = mysqli_fetch_array($query)) {
+                ?>
+                    <tr>
+                      <td><center><?php echo $index ?></td>
+                      <td><center><?php echo $row['username'] ?></td>
+                      <td><center><?php echo $row['alamat'] ?></td>
+                      <td><center><?php echo $row['nomor'] ?></td>
+                      <td><center><?php echo $row['berat'] . " Kg" ?></td>
+                      <td><center><?php echo $row['jenis'] ?></td>
+                      <td><center><?php echo $row['tanggal'] ?></td>
+                      <td><center>Rp.<?php echo $row['jumlah'] ?></td>
+                    </tr>
+                <?php
+                      $index++;
+                    }
+                  }
+                ?>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
   <!--ini Akhir konten bosq-->
 
